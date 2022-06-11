ae8eeda0bc3a617b6a83212270771960faa93437
Yutao-Zhou <13520759678@163.com>
2022-06-11 16:50:31 +0800

    Plot improvement, add Cache
    All stateCode had been replaced with full state name to give user a better experience. Add functionality to select entire USA, or entire state. Add more plot trendline options with parameters and color selection. Improved performance by adding cache to the dataset.

--- bar_map.py
+++ bar_map.py
@@ -3,27 +3,17 @@ import pandas as pd
 import numpy as np
 
 def pydeckMap(data):
-    # data
-    # in this example, the population was scaled down
-    # to change visualization, try changing the elevation_scale and radius arguments
     import pydeck as pdk
-    data['price'] = data['price']/1_000
-    # view (location, zoom level, etc.)
-    view = pdk.ViewState(latitude = 39.155726, longitude = -98.030561, pitch = 50, zoom = 6)
-
-    # layer
+    view = pdk.ViewState(latitude = 39.155726, longitude = -98.030561, pitch = 50, zoom = 4)
     column_layer = pdk.Layer('ColumnLayer',
-                             data=data,
+                             data = data,
                              get_position = ['long', 'lat'],
                              get_elevation = 'price',
-                             elevation_scale = 100,
+                             elevation_scale = 1,
                              radius = 1000,
-                             get_fill_color=[255, 165, 0, 80],
+                             get_fill_color = [204, 255, 255, 80],
                              pickable = True,
                              auto_highlight = True)
-    # render map
-    # with no map_style, map goes to default
-    column_layer_map = pdk.Deck(layers=column_layer, 
-                                initial_view_state=view)
-    # display and save map (to_html(), show())
+    column_layer_map = pdk.Deck(layers = column_layer, 
+                                initial_view_state = view)
     st.pydeck_chart(column_layer_map)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import leafmap.foliumap as leafmap
-import plotly.express as px
+from stateCode2state import name2code, code2name
 
 st.set_page_config(
     page_title="Used Car Analyzer",
@@ -14,19 +14,16 @@ st.set_page_config(
 
 st.title("Used Car Analyzer")
 
-def sidebar(allRegionInCA):
+def sidebar(allRegionInCA, stateCode):
     with st.sidebar:
-        # model = ["gensim"]
-        #
-        # text_input = st.sidebar.text_input("Enter a word you want to query", 'good')
-        # s_station = st.sidebar.selectbox('Select a modole', model, key='usedmodel',
-        #                          help="Which model you want to use to search relevent words?")
-        #
-        # multi_select = st.sidebar.multiselect('multiselection', model, default=["gensim"])
-        # slider_input = st.sidebar.slider('year', 0.01, 1.00, 0.01)
-        area = st.selectbox("select a region you want to view", (allRegionInCA))
-        minPrice = st.number_input("Minimum Listing Price", min_value = 0, value = 1000, step = 1000)
-        maxPrice = st.number_input("Maximum Listing Price", min_value = 0, value = 1000000, step = 1000)
+        if stateCode == "USA":
+            area = "Entire state"
+            minPrice = st.number_input("Minimum Price", min_value = 0, value = 1000, step = 1000)
+            maxPrice = st.number_input("Maximum Price", min_value = 0, value = 100000, step = 1000)
+        else:
+            area = st.selectbox("Select a region you want to view", (allRegionInCA))
+            minPrice = st.number_input("Minimum Price", min_value = 0, value = 1000, step = 1000)
+            maxPrice = st.number_input("Maximum Price", min_value = 0, value = 100000, step = 1000)
     return minPrice, maxPrice, area
 
 def readData(path):
@@ -35,37 +32,63 @@ def readData(path):
         df.dropna(inplace = True)
         df = df.loc[df['price'] != 0]
         df['year'] = df['year'].astype('int64')
-        allState = list(set(df["state"]))
-        allState.append("All States")
+        allStateCode = list(set(df["state"]))
+        allState = []
+        for stateCode in allStateCode:
+            allState.append(code2name(stateCode))
         allState = sorted(allState)
+        allState = ["All States"] + allState
     return df, allState
 
 def selectState(stateCode):
-    state_df = df.loc[df["state"] == stateCode]
-    state_df = state_df.drop(columns=["state"])
+    state_df = df[df["state"] == stateCode]
     return state_df
 
-def countyDataVisualization(state_df):
-    allRegionInCA = set(state_df["region"])
-    minPrice, maxPrice, area = sidebar(allRegionInCA)
-    localCar = state_df.loc[state_df["region"] == area]
+def countyDataVisualization(state_df, stateCode):
+    allRegionInState = sorted(list(set(state_df["region"])) + ['Entire state'])
+    minPrice, maxPrice, area = sidebar(allRegionInState, stateCode)
+    if area == "Entire state":
+        localCar = state_df
+        area = ""
+    else:
+        localCar = state_df[state_df["region"] == area]
+        area = area + ", "
+    localCar = localCar[(localCar['price'] != 0) & (localCar['price'] < maxPrice) & (localCar['price'] > minPrice)]
     n = len(localCar)
-    year = localCar['year'].tolist()
-    price = localCar['price'].tolist()
-    i = 0
-    while i < len(price):
-        if price[i] == 0 or price[i] < minPrice or price[i] > maxPrice:
-            year.pop(i)
-            price.pop(i)
-        else:
-            i += 1
-    averagePrice = sum(price) // len(price)
-    averageYear = sum(year) // len(year)
-    st.write(f"There are {n} listing car in {area}. The average model year is {averageYear}. The average listing price is {averagePrice} USD($).")
-    yearVSprice = pd.DataFrame(data = {"Year of Make":year, "Listing Price of Car in USD($)":price})
+    totalYear = localCar['year'].sum(axis= 0)
+    totalPrice = localCar['price'].sum(axis = 0)
+    averagePrice = totalPrice // n
+    averageYear = totalYear // n
+    st.write(f"There are {n} listed car in {area}{stateCode}. The average model year is {averageYear}. The average price is {averagePrice} USD($).")
+    # yearVSprice = pd.DataFrame(data = {"Year of Make":localCar['year'], "Price of Car in USD($)":localCar['price']})
     with st.expander("Click to view data"):
         st.dataframe(localCar,1000,500)
-    fig = px.scatter(yearVSprice, x = "Year of Make", y = "Listing Price of Car in USD($)", trendline="lowess", trendline_options=dict(frac=0.8))
+    scatterTrend(localCar)
+    return localCar
+
+def scatterTrend(localCar):
+    import plotly.express as px
+    yearVSprice = pd.DataFrame(data = {"Year of Make":localCar['year'], "Price of Car in USD($)":localCar['price']})
+    trendoptions = ["Ordinary Least Squares", "Locally Weighted Scatterplot Smoothing", "Moving Averages", "Expanding mean", "Expanding Maximum"]
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        trendSelected = st.selectbox("Select a trandline you want to use", (trendoptions), index  = 1, )
+    with col2:
+        cScatter = st.color_picker("Pick a color for the scatter", value = "#48D1EA")
+    with col3:
+        cTrend = st.color_picker("Pick a color for the trendline", value = "#FF0000")
+    if trendSelected == "Ordinary Least Squares":
+        fig = px.scatter(yearVSprice, x = "Year of Make", y = "Price of Car in USD($)", color_discrete_sequence=[cScatter], trendline = "ols", trendline_color_override = cTrend)
+    if trendSelected == "Locally Weighted Scatterplot Smoothing":
+        frac = st.slider("Choose a fraction numer for your LOWESS", value = 0.5, min_value = 0.01, max_value = 0.99, step = 0.01, help = "Lowering this fraction will give a line that more closely follows the data")
+        fig = px.scatter(yearVSprice, x = "Year of Make", y = "Price of Car in USD($)", color_discrete_sequence=[cScatter], trendline = "lowess", trendline_options = dict(frac = frac), trendline_color_override = cTrend)
+    if trendSelected == "Moving Averages":
+        window = st.slider("Choose a window numer for your Moving Averages", value = 5, min_value = 1, max_value = 10, step = 1)
+        fig = px.scatter(yearVSprice, x = "Year of Make", y = "Price of Car in USD($)", color_discrete_sequence=[cScatter],  trendline="rolling", trendline_options=dict(window = window), trendline_color_override = cTrend)
+    if trendSelected == "Expanding mean":
+        fig = px.scatter(yearVSprice, x = "Year of Make", y = "Price of Car in USD($)", color_discrete_sequence=[cScatter], trendline = "expanding", trendline_color_override = cTrend)
+    if trendSelected == "Expanding Maximum":
+        fig = px.scatter(yearVSprice, x = "Year of Make", y = "Price of Car in USD($)", color_discrete_sequence=[cScatter], trendline = "expanding", trendline_options=dict(function="max"), trendline_color_override = cTrend)
     st.plotly_chart(fig, use_container_width = True)
 
 def listingMap(data):
@@ -77,21 +100,23 @@ def mapOfAveragePrice():
     m.add_basemap("OpenStreetMap")
     m.to_streamlit(weight = 1000, height = 600)
 
-@st.cache(allow_output_mutation = True)
+@st.cache(allow_output_mutation = True, show_spinner = False)
 def load_model(path):
     df, allState = readData(path)
     return df, allState
 
 # df, allState = readData("C:/Users/13520/Documents/GitHub/Used_Car_Analysis/used_car_us.csv")
 df, allState = load_model("C:/Users/13520/Documents/GitHub/Used_Car_Analysis/used_car_us.csv")
-
-stateCode = st.sidebar.selectbox("select a region you want to view", (allState))
-if stateCode == "All States":
-    state_df = df
+stateName = st.sidebar.selectbox("Select a state you want to view", (allState))
+if stateName == "All States":
+    with st.container():
+        localcar = countyDataVisualization(df, "USA")
+        listingMap(localcar)
 else:
+    stateCode = name2code(stateName)
     state_df = selectState(stateCode)
-with st.container():
-    countyDataVisualization(state_df)
-    listingMap(state_df)
+    with st.container():
+        localcar = countyDataVisualization(state_df, stateName)
+        listingMap(localcar)
 # mapOfAveragePrice()
 # geocoder("santa barbara", "ca")
